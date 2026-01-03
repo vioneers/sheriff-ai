00df5732038efe825eecfa966bf99efee4dd6d6c
Maria NEAGOIE <maria.neagoie@polytechnique.edu>
2026-01-03 19:40:38 +0100

    Time limit changed to 9.5s (9.8 bound had timeouts)

--- src/main_lichess.cpp
+++ src/main_lichess.cpp
@@ -22,7 +22,7 @@ static string compute_bestmove(const vector<move_t>& move_hist, const string& fe
 
     using clock = std::chrono::steady_clock;
     engine.start_time = clock::now();
-    engine.time_limit = std::chrono::milliseconds(9800); // ~9.8 seconds
+    engine.time_limit = std::chrono::milliseconds(9500); // ~9.5 seconds
     engine.time_up = false;
 
     Color turn = engine.board.history.back().turn;
